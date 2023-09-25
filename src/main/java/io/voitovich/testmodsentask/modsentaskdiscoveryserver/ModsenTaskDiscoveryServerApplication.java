e3a1dcf8b351c8be5ccab9c6f02411dd58eb15d6
voito <voitovich200233@gmail.com>
2023-09-25 16:46:54 +0300

    Initial commit

---
+++ src/main/java/io/voitovich/testmodsentask/modsentaskdiscoveryserver/ModsenTaskDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package io.voitovich.testmodsentask.modsentaskdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsenTaskDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsenTaskDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/voitovich/testmodsentask/modsentaskdiscoveryserver/ModsenTaskDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.voitovich.testmodsentask.modsentaskdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModsenTaskDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
