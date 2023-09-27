0a21036087d24288d21ef8ef56561d327f8e0362
voito <voitovich200233@gmail.com>
2023-09-27 20:09:39 +0300

    discovery server logic implemented

--- src/main/java/io/voitovich/testmodsentask/modsentaskdiscoveryserver/ModsenTaskDiscoveryServerApplication.java
+++ src/main/java/io/voitovich/testmodsentask/modsentaskdiscoveryserver/ModsenTaskDiscoveryServerApplication.java
@@ -2,12 +2,15 @@ package io.voitovich.testmodsentask.modsentaskdiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class ModsenTaskDiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModsenTaskDiscoveryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ModsenTaskDiscoveryServerApplication.class, args);
+    }
 
 }
